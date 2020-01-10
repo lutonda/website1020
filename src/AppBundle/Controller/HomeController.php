b3c24f8ffa908f79bc67721cb9b4da7bff96e2e4
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-01-10 13:55:17 +0000

    templating

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+    /**
+     * @Route("/home" )
+     */
+class HomeController extends Controller
+{
+    /**
+     * @Route("/start", name="home_homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('home/index.html', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
